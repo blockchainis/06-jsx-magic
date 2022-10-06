a05c3ae85531e66b3be026228a809be16d61e9c3
hoon2 <sanghoon.engineer@gmail.com>
2022-10-06 11:27:02 +0900

    06 시작 코드

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 function App() {
-  return <></>;
+  return (
+    <div>
+      <h2>LastName: 한</h2>
+      <h2>FirstName: 상훈</h2>
+      <h2>FullName: 한상훈</h2>
+    </div>
+  );
 }
 
 export default App;
